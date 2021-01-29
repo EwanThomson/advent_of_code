abf35d88ca4fdd001abc3e649d6e02d87f356a11
EwanThomson <67035332+EwanThomson@users.noreply.github.com>
2021-01-28 20:40:39 -0800

    2020 day 3 part 2

--- aoc2020/src/Day3.java
+++ aoc2020/src/Day3.java
@@ -7,16 +7,33 @@ public class Day3 {
 
     public static void main(String[] args) {
         List<String> lines = readInput("/Users/ewan/Documents/GitHub/advent_of_code/day3_input.txt");
+        int numTrees[] = {
+                getTrees(lines, 1, 1),
+                getTrees(lines, 3, 1),
+                getTrees(lines, 5, 1),
+                getTrees(lines, 7, 1),
+                getTrees(lines, 1, 2),
+        };
+        long ans = 1;
+        for (int numTree : numTrees) {
+            System.out.print(numTree + " ");
+            ans *= numTree;
+        }
+        System.out.println(ans);
+    }
+
+    static int getTrees(List<String> lines, int right, int down) {
         char tree = '#';
         int column = 0;
         int trees = 0;
-        for (String line : lines) {
+        for (int row = 0; row < lines.size() / down; row++) {
+            String line = lines.get(down * row);
             if (line.charAt(column % line.length()) == tree) {
                 trees++;
             }
-            column += 3;
+            column += right;
         }
-        System.out.println(trees);
+        return trees;
     }
 
     static List<String> readInput(String path) {
